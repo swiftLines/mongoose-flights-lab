f05c27e28114d6e66cd33921403fb1eae01fe7a5
Jeremy Underwood <underwood.j248@gmail.com>
2022-02-28 08:46:47 -0500

    set validations

--- models/flight.js
+++ models/flight.js
@@ -4,10 +4,26 @@ import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 
 const flightSchema = new Schema({
-  airline: String,
-  airport: String,
-  flightNo: Number,
-  departs: Date
+  airline: {
+    type: String,
+    required: true,
+    enum: ['American', 'Southwest', 'United']
+  },
+  airport: {
+    type: String,
+    required: true,
+    enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN']
+  },
+  flightNo: {
+    type: Number,
+    required: true,
+    min: 10,
+    max: 9999
+  },
+  departs: {
+    type: Date,
+    required: true
+  },
 })
 
 // compile the schema into a model and export it
